2a3c82f40ad1b9d8e5b680273428657b4b08f7fd
cjean-pierre <cjeanpierre.github@gmail.com>
2022-09-28 10:40:44 +0200

    Updated main.py

--- Data_Scientist/P7_Scoring/main.py
+++ Data_Scientist/P7_Scoring/main.py
@@ -1,66 +1,21 @@
-import time
-from datetime import timedelta
-import re
-from contextlib import contextmanager
-
-
-from prev_app import *
-from pos import *
-from bureau import*
-from credit_card import*
-from installments import *
-from application import*
+from preprocess import *
 from lightGBM import*
 
 
-@contextmanager
-def timer(title):
-    t0 = time.time()
-    yield
-    t_delta = str(timedelta(seconds=time.time()-t0)).split(':')
-    print("{} - done in {}h {}min {}sec".format(title, t_delta[0], t_delta[1], t_delta[2]))
+def main():
 
-
-def main(debug=False):
-    df = application_train_test()
-    print("Application df shape:", df.shape)
-    with timer("Process bureau and bureau_balance"):
-        bureau = bureau_and_balance()
-        print("Bureau df shape:", bureau.shape)
-        df = df.merge(bureau, how='left', on='SK_ID_CURR')
-        del bureau
-
-    with timer("Process previous_applications"):
-        prev = previous_applications()
-        print("Previous applications df shape:", prev.shape)
-        df = df.merge(prev, how='left', on='SK_ID_CURR')
-        del prev
-
-    with timer("Process POS-CASH balance"):
-        pos = pos_cash()
-        print("Pos-cash balance df shape:", pos.shape)
-        df = df.merge(pos, how='left', on='SK_ID_CURR')
-        del pos
-
-    with timer("Process installments payments"):
-        ins = installments_payments()
-        print("Installments payments df shape:", ins.shape)
-        df = df.merge(ins, how='left', on='SK_ID_CURR')
-        del ins
-
-    with timer("Process credit card balance"):
-        cc = credit_card_balance()
-        print("Credit card balance df shape:", cc.shape)
-        df = df.merge(cc, how='left', on='SK_ID_CURR')
-        del cc
+    with timer("Run preprocessing"):
+        train_df, test_df = preprocessing()
 
     with timer("Run LightGBM with kfold"):
-        df.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x), inplace=True)
-        feat_importance, prediction, contrib_stats, train_preds = kfold_lightgbm(df, num_folds=5,
-                                                                                 stratified=True, debug=debug)
-        feat_importance.to_csv('best_features.csv')
-        contrib_stats.to_csv('contributions.csv')
-        train_preds.to_csv('train_preds_analysis.csv')
+
+        feat_importance, oof_preds, _ = kfold_lightgbm(train_df, test_df, num_folds=5,
+                                                       smote=False, class_weight={0: 1, 1: 6},
+                                                       vis=True, contrib=False)
+
+        score_lightgbm(oof_preds, train_df['TARGET'], threshold=0.3, beta=1, verbose=True)
+
+        display_importances(feat_importance, save_files=False)
 
 
 with timer("Full model run"):
